423c3486612b2a9e7f795d2ead152aa7c71b46f1
exvak-main-api <sigmagang2025@gmail.com>
2026-07-15 09:20:59 +0100

    Update Function.cpp

--- objects/Function.cpp
+++ objects/Function.cpp
@@ -1,23 +1,22 @@
 #include "Function.hpp"
 
+
 namespace NovaLuau
 {
 
 
 Function::Function(
     const std::string& name,
-    const std::vector<std::string>& parameters,
-    const std::vector<std::string>& body,
-    const std::string& source,
-    int lineDefined
+    NativeFunction callback
 )
 {
+
     this->name = name;
-    this->parameters = parameters;
-    this->body = body;
-    this->source = source;
-    this->lineDefined = lineDefined;
-    this->isNative = false;
+
+    this->nativeCallback = callback;
+
+    this->isNative = true;
+
 }
 
 
@@ -26,32 +25,35 @@ Function::Function(
     const std::string& name
 )
 {
+
     this->name = name;
-    this->source = "[C]";
-    this->lineDefined = 0;
-    this->isNative = true;
+
+    this->isNative = false;
+
 }
 
 
 
 Value Function::call(
-    std::vector<Value> args,
-    std::shared_ptr<Environment> environment
+    std::vector<Value> args
 )
 {
-    auto local = std::make_shared<Environment>(environment);
 
-
-    for(size_t i = 0; i < parameters.size(); i++)
+    if(
+        isNative &&
+        nativeCallback
+    )
     {
-        if(i < args.size())
-            local->define(parameters[i], args[i]);
-        else
-            local->define(parameters[i], Value());
+
+        return nativeCallback(
+            args
+        );
+
     }
 
 
     return Value();
+
 }
 
 
@@ -63,48 +65,9 @@ std::string Function::getName()
 
 
 
-std::string Function::getSource()
-{
-    return source;
-}
-
-
-
-int Function::getLineDefined()
-{
-    return lineDefined;
-}
-
-
-
 bool Function::native()
 {
     return isNative;
-}
-
-
-
-std::string Function::getInfo()
-{
-    std::string info;
-
-    info += "source = " + source + "\n";
-    info += "short_src = " + source.substr(0, 60) + "\n";
-    info += "linedefined = " + std::to_string(lineDefined) + "\n";
-    info += "what = ";
-
-    if(isNative)
-        info += "C\n";
-    else
-        info += "Lua\n";
-
-
-    info += "name = " + name + "\n";
-    info += "namewhat = global\n";
-    info += "nups = 0";
-
-
-    return info;
 }
 
 
