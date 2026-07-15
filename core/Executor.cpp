b6e585fabb462ec77cad7daa0d141829a699029e
exvak-main-api <sigmagang2025@gmail.com>
2026-07-15 09:18:52 +0100

    Update Executor.cpp

--- core/Executor.cpp
+++ core/Executor.cpp
@@ -1,5 +1,9 @@
 #include "Executor.hpp"
+
+#include "../parser/AST.hpp"
+
 #include <iostream>
+
 
 namespace NovaLuau
 {
@@ -14,6 +18,83 @@ Executor::Executor(
 
 
 
+Value evaluate(
+    std::shared_ptr<ASTNode> node,
+    std::shared_ptr<Environment> environment
+)
+{
+
+    if(
+        node->type == ASTType::Number
+    )
+    {
+        auto value =
+            std::static_pointer_cast<ValueNode>(node);
+
+        return Value(
+            std::stod(value->value)
+        );
+    }
+
+
+
+    if(
+        node->type == ASTType::String
+    )
+    {
+        auto value =
+            std::static_pointer_cast<ValueNode>(node);
+
+        return Value(
+            value->value
+        );
+    }
+
+
+
+    if(
+        node->type == ASTType::Boolean
+    )
+    {
+        auto value =
+            std::static_pointer_cast<ValueNode>(node);
+
+        return Value(
+            value->value == "true"
+        );
+    }
+
+
+
+    if(
+        node->type == ASTType::Nil
+    )
+    {
+        return Value();
+    }
+
+
+
+    if(
+        node->type == ASTType::Variable
+    )
+    {
+        auto variable =
+            std::static_pointer_cast<VariableNode>(node);
+
+        return environment->get(
+            variable->name
+        );
+    }
+
+
+
+    return Value();
+
+}
+
+
+
 void Executor::execute(
     std::vector<std::shared_ptr<ASTNode>> nodes
 )
@@ -22,22 +103,78 @@ void Executor::execute(
     for(auto& node : nodes)
     {
 
-        if(!node)
+
+        if(
+            node->type ==
+            ASTType::VariableDeclaration
+        )
+        {
+
+            auto declaration =
+                std::static_pointer_cast<
+                    VariableDeclarationNode
+                >(node);
+
+
+            Value value =
+                evaluate(
+                    declaration->value,
+                    environment
+                );
+
+
+            environment->define(
+                declaration->name,
+                value
+            );
+
+
             continue;
 
+        }
 
-        /*
-            AST execution will be implemented here.
 
-            This layer will handle:
 
-            - variables
-            - functions
-            - tables
-            - calls
-            - returns
-            - expressions
-        */
+        if(
+            node->type ==
+            ASTType::Call
+        )
+        {
+
+            auto call =
+                std::static_pointer_cast<
+                    CallNode
+                >(node);
+
+
+            if(
+                call->name == "print"
+            )
+            {
+
+                if(
+                    !call->arguments.empty()
+                )
+                {
+
+                    Value value =
+                        evaluate(
+                            call->arguments[0],
+                            environment
+                        );
+
+
+                    std::cout
+                        << value.toString()
+                        << std::endl;
+
+                }
+
+
+            }
+
+
+        }
 
 
     }
